89a8bb7f3021faf2f45b6a52ec634b67802f0336
Amy Baker <amyraebaker@gmail.com>
2022-04-02 12:55:38 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -10,4 +10,11 @@ var countSheep = function (num){
     let str = "";
     for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
     return str;
-  }
+}
+
+function firstNonConsecutive (arr) {
+    for(let i=1; i<arr.length; i++) {
+      if (arr[i] !== arr[i-1]+1) return arr[i]
+    }
+    return null
+}

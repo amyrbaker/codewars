a00cd04d3aa03909e644f677997ffebffb1a845b
Amy Baker <amyraebaker@gmail.com>
2022-04-03 00:31:43 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -50,3 +50,11 @@ function datingRange(age){
 function addLength(str) {
     return str.split(' ').map(w => `${w} ${w.length}`)
 }
+
+function charFreq(message) {
+    let obj = {}
+    for (let i = 0; i < message.length; i++) {
+      !obj[message[i]] ? obj[message[i]] = 1 : obj[message[i]]++
+    }
+    return obj
+}

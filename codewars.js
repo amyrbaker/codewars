8d0d2ffc2c9763c15e119c6395b1be48723b160d
Amy Baker <amyraebaker@gmail.com>
2022-04-04 00:05:43 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -91,3 +91,7 @@ function multiply(number){
     return number * 5 ** Math.abs(number).toString().length
 }
 
+function evil(n) {
+    return n.toString(2).split('').filter(n => n == 1).length % 2 === 0 ? 'It\'s Evil!' : 'It\'s Odious!'
+}
+

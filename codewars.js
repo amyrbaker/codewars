f8f06bbbb3dcf180a5d1bd4eb05537f767d6d45b
Amy Baker <amyraebaker@gmail.com>
2022-04-05 23:09:09 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -217,3 +217,8 @@ function cutCube(volume,n){
     
 }
 
+function blackAndWhite(arr){
+    return !Array.isArray(arr) ? 'It\'s a fake array' :
+      arr.indexOf(5) !== -1 && arr.indexOf(13) !== -1 ? 'It\'s a black array' :
+      'It\'s a white array'
+}

fb73509f79f86f83a8fcdfae07e2f37bbffe59b4
Amy Baker <amyraebaker@gmail.com>
2022-04-05 23:32:12 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -237,3 +237,11 @@ function firstToLast(str,c){
     
 }
 
+function rainAmount (mm) {
+    if (mm < 40) {
+      return `You need to give your plant ${40 - mm}mm of water`;
+    } else {
+      return "Your plant has had more than enough water for today!";
+    }
+}
+

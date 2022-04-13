81d0473033359c26d2ed31b2b84af00b14b8d4a4
Amy Baker <amyraebaker@gmail.com>
2022-04-13 14:50:20 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -304,3 +304,16 @@ function whatNumberIsIt(n){
     
 }
 
+function giveMeFive(obj){
+  let five = [];
+  
+  for (let key in obj) {
+    if (key.length === 5) five.push(key);
+    if (obj[key].length === 5) five.push(obj[key]);
+  }
+  
+  return five;
+}
+
+
+

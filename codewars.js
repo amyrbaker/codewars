4af08dd5d826e955149d35e62106910015bd3b4b
Amy Baker <amyraebaker@gmail.com>
2022-04-13 23:54:18 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -360,5 +360,24 @@ function padIt(str,n){
   return string
 }
 
+function getLength(arr){
+  return arr.length
+}
+function getFirst(arr){
+  return arr[0]
+}
+function getLast(arr){
+  return arr[arr.length - 1]
+}
+function pushElement(arr){
+  var el=1;
+  arr.push(el)
+  
+  return arr
+}
+function popElement(arr){
+  return arr.slice(0, -1)
+}
+
 
 

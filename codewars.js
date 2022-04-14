29ff93b45321b601d837a24b83af2b5e8cdac574
Amy Baker <amyraebaker@gmail.com>
2022-04-13 23:43:34 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -333,5 +333,18 @@ function grabDoll(dolls){
   return bag;
 }
 
+function pickIt(arr){
+  var odd=[],
+      even=[];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+  return [odd,even];
+}
+
 
 

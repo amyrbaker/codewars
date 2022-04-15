63426c565b5c27c2ffa34933d7320fba28b7fe35
Amy Baker <amyraebaker@gmail.com>
2022-04-15 12:43:36 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -29,3 +29,12 @@ function arrAdder(arr) {
     return str.trim()
 }
 
+function mxdiflg(a1, a2) {
+    let a1Length = a1.map(w => w.length).sort((a, b) => a - b)
+    let a1MM = [a1Length[0], a1Length[a1Length.length - 1]]
+    let a2Length = a2.map(w => w.length)
+    let a2MM = [Math.min(...a2Length), Math.max(...a2Length)]
+    console.log(a1MM, a2MM)
+    return (a1.length === 0 || a2.length === 0) ? -1 : Math.max(Math.abs(a1MM[0] - a2MM[0]), Math.abs(a1MM[0] - a2MM[1]), Math.abs(a1MM[1] - a2MM[0]), Math.abs(a1MM[1] - a2MM[1]))
+}
+

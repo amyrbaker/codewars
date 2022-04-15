368632b7ce9fd5c446a9914c73b85c3b9499b37b
Amy Baker <amyraebaker@gmail.com>
2022-04-14 18:15:12 -0700

    Complete Kata

--- codewars.js
+++ codewars.js
@@ -391,5 +391,15 @@ function numberToPower(number, power){
   }
 }
 
+Array.prototype.filter = function (func) {
+  let arr = []
+  for (let i = 0; i < this.length; i++) {
+    if (func(this[i])) {
+      arr.push(this[i])
+    }
+  }
+  return arr
+}
+
 
 

7eba2439e025995f6617b40673c63f98e0c02692
Amy Baker <amyraebaker@gmail.com>
2022-04-15 12:27:13 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -18,3 +18,14 @@ function vowelIndices(word){
     return arr
 }
 
+function arrAdder(arr) {
+    let str = ''
+    for (let i = 0; i < arr[0].length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        str += arr[j][i]
+      }
+      str += ' '
+    }
+    return str.trim()
+}
+

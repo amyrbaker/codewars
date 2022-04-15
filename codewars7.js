10124c6a60824d19aead3f77f1528a4ede7ade4b
Amy Baker <amyraebaker@gmail.com>
2022-04-15 12:17:27 -0700

    Complete Kata

---
+++ codewars7.js
@@ -0,0 +1,9 @@
+function unusedDigits(...arg) {
+    let arr = []
+    for (let i = 0; i <= 9; i++) {
+      if (!arg.join('').includes(i)) {
+        arr.push(i)
+      }
+    }
+    return arr.join('')
+  }

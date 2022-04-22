6b140dde070b6ac73c50c233aad49779f946c5bb
Amy Baker <amyraebaker@gmail.com>
2022-04-21 21:38:46 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -336,6 +336,13 @@ function calculateYears(principal, interest, tax, desired) {
   
 }
 
+function arithmetic(a, b, operator){
+  return operator === 'add' ? a + b :
+    operator === 'subtract' ? a - b :
+    operator === 'divide' ? a / b :
+    a * b
+}
+
 
 
 

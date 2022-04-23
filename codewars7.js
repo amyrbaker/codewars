111eb9e5382372d7c6022fc58638a713774cb247
Amy Baker <amyraebaker@gmail.com>
2022-04-23 11:43:11 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -482,6 +482,20 @@ function gimme (triplet) {
   return triplet.map((e, i) => [e, i]).sort((a, b) => a[0] - b[0])[1][1]
 }
 
+function checkExam(array1, array2) {
+  let score = 0;
+   for (let i=0;i<array1.length;i++){
+     if (array1[i] === array2[i]){
+       score += 4;
+     } else if (array2[i] === ''){
+       score;
+     } else {
+       score -= 1;
+     }
+   }
+   return score < 0 ? 0 : score;
+ }
+
 
 
 

8ac51f63846c1d15eec8378e5ff2c2449f182ab9
Amy Baker <amyraebaker@gmail.com>
2022-04-28 17:38:36 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -560,6 +560,10 @@ function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
   return Math.floor(Math.sqrt(age1**2+age2**2+age3**2+age4**2+age5**2+age6**2+age7**2+age8**2) / 2)
 }
 
+function sumOfMinimums(arr) {
+  return arr.map(array => array.sort((a, b) => a - b)[0]).reduce((a, c) => a + c, 0)
+}
+
 
 
 

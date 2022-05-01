6201e452cbb6eb885c3f049985fe27a267e6be62
Amy Baker <amyraebaker@gmail.com>
2022-05-01 14:33:43 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -192,6 +192,17 @@ function high(x){
   return xArr[index]
 }
 
+function bouncingBall(h,  bounce,  window) {
+  if (!(h > 0 && bounce > 0 && bounce < 1 && window < h)) return -1
+  let count = 1
+  let newHeight = h * bounce
+  while (newHeight > window) {
+    count += 2
+    newHeight *= bounce
+  }
+  return count
+}
+
 
 
 

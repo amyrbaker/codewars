93e3f7d7c6a76ebfbea27e47c78930da522f6a94
Amy Baker <amyraebaker@gmail.com>
2022-05-02 17:58:52 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -244,6 +244,14 @@ function wave(str){
   return arr
 }
 
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j]
+    }
+  }
+}
+
 
 
 

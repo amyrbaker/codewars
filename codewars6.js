bb79cfa662159075db3a408409883bc801ccce52
Amy Baker <amyraebaker@gmail.com>
2022-05-02 17:51:36 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -236,6 +236,14 @@ function count (string) {
   }, {})
 }
 
+function wave(str){
+  let arr = []
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') arr.push(str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1))
+  }
+  return arr
+}
+
 
 
 

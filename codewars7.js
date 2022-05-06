959fce7649c7562042a12667c63b2c515ffa3ad3
Amy Baker <amyraebaker@gmail.com>
2022-05-06 15:05:38 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -652,6 +652,20 @@ function alphabetWar(fight)
   return leftScore > rightScore ? 'Left side wins!' : leftScore < rightScore ? 'Right side wins!' : 'Let\'s fight again!'
 }
 
+// TODO: implement the four functions specified.
+function head(arr) {
+  return arr[0]
+}
+function tail(arr) {
+  return arr.slice(1)
+}
+function init(arr) {
+  return arr.slice(0, -1)
+}
+function last(arr) {
+  return arr[arr.length - 1]
+}
+
 
 
 

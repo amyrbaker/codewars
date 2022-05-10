374f9a4ab578226d664f99a0b1efbe0c1fdf10db
Amy Baker <amyraebaker@gmail.com>
2022-05-10 01:31:20 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -366,6 +366,15 @@ var encryptThis = function(text) {
   }).join(' ')
 }
 
+function cleanString(s) {
+  let arr = []
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '#') arr.pop()
+    else arr.push(s[i])
+  }
+  return arr.join('')
+}
+
 
 
 

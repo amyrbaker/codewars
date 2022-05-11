a5fca816059ab52c9bb5e568eb26670d3499d9a4
Amy Baker <amyraebaker@gmail.com>
2022-05-11 00:35:55 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -440,6 +440,18 @@ function minPermutation(n) {
   return n < 0 ? -1 * +numb : +numb
 }
 
+function getDay(day, isLeap){
+  let months = {1 : 'January', 2 : 'February', 3 : 'March', 4 : 'April', 5 : 'May', 6 : 'June', 7 : 'July', 8 : 'August', 9 : 'September', 10 : 'October', 11 : 'November', 12 : 'December'}
+  let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if (isLeap) days[1] = 29
+  let currentMonth = 1
+  while (day > days[currentMonth - 1]) {
+    day -= days[currentMonth - 1]
+    currentMonth++
+  }
+  return `${months[currentMonth]}, ${day}`
+}
+
 
 
 

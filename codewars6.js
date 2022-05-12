751ba24ea7b302d1d9906938f5156d0267624bc2
Amy Baker <amyraebaker@gmail.com>
2022-05-12 16:55:36 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -541,6 +541,14 @@ function meeting(s) {
     .join('')
 }
 
+function dataReverse(data) {
+  let bytes = []
+  for (let i = 0; i < data.length; i += 8) {
+    bytes.push(data.slice(i, i + 8))
+  }
+  return bytes.reverse().reduce((a, c) => a.concat(c), [])
+}
+
 
 
 

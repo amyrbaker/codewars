f62cdfcc9fb3fad78017710c0f2bf39d12747c1a
Amy Baker <amyraebaker@gmail.com>
2022-05-15 23:50:53 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -647,6 +647,25 @@ function dashatize(num) {
   return arr.join('')
 }
 
+Array.prototype.square = function() {
+  return this.map(e => e ** 2)
+}
+Array.prototype.cube = function() {
+  return this.map(e => e ** 3)
+}
+Array.prototype.average = function() {
+  return this.length ? this.reduce((a, c) => a + c, 0) / this.length : NaN
+}
+Array.prototype.sum = function() {
+  return this.reduce((a, c) => a + c, 0)
+}
+Array.prototype.even = function() {
+  return this.filter(e => !(e % 2))
+}
+Array.prototype.odd = function() {
+  return this.filter(e => e % 2)
+}
+
 
 
 

f02c8983c2f2d3a3c85a1da6a8595f46702bf2e3
Amy Baker <amyraebaker@gmail.com>
2022-05-19 19:49:41 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -841,6 +841,30 @@ class FileNameExtractor {
   }
 }
 
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function length(head) {
+  let temp = head, count = 0
+  while (temp !== null) {
+    count++
+    temp = temp.next
+  }
+  return count
+}
+
+function count(head, data) {
+  let temp = head, count = 0
+  while (temp !== null) {
+    console.log(temp)
+    if (temp.data == data) count++
+    temp = temp.next
+  }
+  return count
+}
+
 
 
 

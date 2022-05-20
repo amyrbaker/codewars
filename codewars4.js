6e91a0409431e588c8233c20d81ea600b4e2f011
Amy Baker <amyraebaker@gmail.com>
2022-05-19 22:41:06 -0700

    Complete Kata

---
+++ codewars4.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+    let arr = []
+    a = a.padStart(Math.max(a.length, b.length), '0')
+    b = b.padStart(Math.max(a.length, b.length), '0')
+    let rem = 0
+    for (let i = a.length - 1; i >= 0; i--) {
+      let sum = +a[i] + +b[i] + rem
+      arr.unshift(sum % 10)
+      rem = Math.floor(sum / 10)
+    }
+    rem !== 0 ? arr.unshift(rem) : arr
+    return arr.join('')
+  }
+

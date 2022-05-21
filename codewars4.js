55e104bf7beba007c3ce8faad63e656f813d6d08
Amy Baker <amyraebaker@gmail.com>
2022-05-21 13:36:18 -0700

    Complete Kata

--- codewars4.js
+++ codewars4.js
@@ -16,3 +16,43 @@ function multiply(a, b){
     return (BigInt(a) * BigInt(b)) + ''
 }
 
+function differentiate(equation, point){
+    let signs = []
+    for (let i = 1; i < equation.length; i++) {
+      if (equation[i] === '+' || equation[i] === '-') signs.push(equation[i])
+    }
+    let neg = equation[0] === '-' ? true : false
+    let arr = neg ? equation.slice(1).split(/[+,-]/) : equation.split(/[+,-]/)
+    if (neg) arr[0] = '-' + arr[0]
+    arr = arr.map(e => {
+      return e.includes('^') ? e.split('^') : 
+        (e.includes('x') && e.length > 1) ? e.slice(0, e.length - 1) : 
+        e.includes('x') ? '1' : '0'
+    })
+    arr = arr.map(e => {
+      if (e[0] === 'x') {
+        e[0] = `${e[1]}x`
+        e[1] = (+e[1] - 1) + ''
+      } else if (e[0] === '-x') {
+        e[0] = `${(-1 * +e[1]) + ''}x`
+        e[1] = (+e[1] - 1) + ''
+      } else if (e[0].includes('x')) {
+        e[0] = `${(+(e[0].slice(0, e[0].length - 1)) * +e[1]) + ''}x`
+        e[1] = (+e[1] - 1) + ''
+      } else e
+      return e
+    })
+    arr = arr.map(e => {
+      if (e[0].includes('x') && +e[1] >= 1) {
+        e = (point ** +e[1]) * +e[0].slice(0, e[0].length - 1)
+      } else e = +e
+      return e
+    })
+    let sum = arr[0]
+    for (let i = 0; i < signs.length; i++) {
+      if (signs[i] === '+') sum += arr[i + 1]
+      else if (signs[i] === '-') sum -= arr[i + 1]
+    }
+    return sum
+}
+

efb5859497c46122e0fb13c98808140a6aee95ea
Amy Baker <amyraebaker@gmail.com>
2022-06-05 15:51:24 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -1217,6 +1217,16 @@ function solution(n){
   return Math.round(n * 2) / 2
 }
 
+function simpleTransposition(text) {
+  let row1 = []
+  let row2 = []
+  for (let i = 0; i < text.length; i++) {
+    if (!(i % 2)) row1.push(text[i])
+    else row2.push(text[i])
+  }
+  return row1.concat(row2).join('')
+}
+
 
 
 

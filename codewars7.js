83dfcc8594e1ef143c43acc17bde77c25bdde44d
Amy Baker <amyraebaker@gmail.com>
2022-07-01 23:49:40 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -716,6 +716,18 @@ function explode(s) {
   return str
 }
 
+function vertMirror(strng) {
+  return strng.split('\n').map(e => e.split('').reverse().join('')).join('\n')
+}
+function horMirror(strng) {
+  return strng.split('\n').reverse().join('\n')
+}
+function oper(fct, s) {
+  return fct(s)
+}
+
+
+
 
 
 

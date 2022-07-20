ed26432d33200ed47a09ca9d8b15229a40498ac6
Amy Baker <amyraebaker@gmail.com>
2022-07-20 13:29:47 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -887,6 +887,14 @@ function calc(x){
   return one - total2
 }
 
+function pairs(ar){
+  let count = 0
+  for (let i = 0; i < ar.length - 1; i+= 2) {
+    if (Math.abs(ar[i] - ar[i + 1]) === 1) count++
+  }
+  return count
+};
+
 
 
 

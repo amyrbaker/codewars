f463ff49b6db42272b1ddd93609d448936424dbe
Amy Baker <amyraebaker@gmail.com>
2022-08-23 21:30:38 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -1849,6 +1849,18 @@ function mirror(text){
   return arr.join('\n')
 }
 
+function total(arr){
+  return arr.filter((e, i) => isPrime(i)).reduce((a, c) => a + c, 0)
+};
+
+function isPrime(n) {
+  if (n < 2) return false
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false
+  }
+  return true
+}
+
 
 
 

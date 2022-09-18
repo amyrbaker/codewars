06d859e47e7b348b69458dbedbdebb5d2e601fc0
Amy Baker <amyraebaker@gmail.com>
2022-09-17 22:49:45 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -1230,6 +1230,12 @@ function getSumOfDigits(integer) {
   return sum;
 }
 
+function toTime(seconds) {
+  let hours = Math.floor(seconds / 3600)
+  let minutes = Math.floor(seconds % 3600 / 60)
+  return `${hours} hour(s) and ${minutes} minute(s)`
+}
+
 
 
 

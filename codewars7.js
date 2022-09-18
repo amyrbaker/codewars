89c5a86379d682983be5aa96f46522f48cfb5f7f
Amy Baker <amyraebaker@gmail.com>
2022-09-17 22:46:48 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -1221,6 +1221,15 @@ var removeVowels = function(str){
   return str.split('').filter(e => !'aeiou'.includes(e)).join('')
 }
 
+function getSumOfDigits(integer) {
+  var sum = 0;
+  var digits =  Math.floor(integer).toString();
+  for(var ix = 0; ix < digits.length; ix++) {
+    sum += +digits[ix];
+  }
+  return sum;
+}
+
 
 
 

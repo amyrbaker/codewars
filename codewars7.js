12df33b3e157f90816f9bcc877b4ffe2b4b9f861
Amy Baker <amyraebaker@gmail.com>
2022-09-19 21:54:51 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -1261,6 +1261,14 @@ function productArray(numbers){
   return arr
 }
 
+var splitInParts = function(s, partLength){
+  let arr = []
+  for (let i = 0; i < s.length; i += partLength) {
+    arr.push(s.slice(i, i + partLength))
+  }
+  return arr.join(' ')
+}
+
 
 
 

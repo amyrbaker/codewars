64a539a934e2da4a2146b5544922393f571869ef
Amy Baker <amyraebaker@gmail.com>
2022-09-27 23:03:37 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -1407,6 +1407,10 @@ function average(scores) {
   return Math.round(scores.reduce((a, c) => a + c, 0) / scores.length)
 }
 
+function greet(name) {
+  return name ? `hello ${name}!` : null
+}
+
 
 
 

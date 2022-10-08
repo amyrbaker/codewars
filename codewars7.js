435d0b26b4586a6e090ad223046381fa9b145bca
Amy Baker <amyraebaker@gmail.com>
2022-10-07 22:51:55 -0700

    Complete kata

--- codewars7.js
+++ codewars7.js
@@ -1927,6 +1927,15 @@ function isDivisible(...args){
   return rest.every(e => first % e === 0)
 }
 
+function consecutive(arr) {
+  let count = 0
+  arr = arr.sort((a, b) => a - b)
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + 1 !== arr[i + 1]) count += arr[i + 1] - arr[i] - 1
+  }
+  return count
+}
+
 
 
 

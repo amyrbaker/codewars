03830830779ec1dc65a2f04c27a76c2540e8f0ea
Amy Baker <amyraebaker@gmail.com>
2022-10-14 23:50:36 -0700

    Complete Kata

--- codewars7.js
+++ codewars7.js
@@ -2150,6 +2150,10 @@ var circleArea = function(radius) {
   return +(Math.PI * radius * radius).toFixed(2)
 };
 
+function solve(a){
+  return a.filter(e => e % 2 === 0).length - a.filter(e => e % 2).length
+};
+
 
 
 

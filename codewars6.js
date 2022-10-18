365155191914d0c2ee45c0b03c347b426995b12e
Amy Baker <amyraebaker@gmail.com>
2022-10-18 13:50:13 -0700

    Complete Kata

--- codewars6.js
+++ codewars6.js
@@ -2142,6 +2142,15 @@ function findUnique(numbers) {
     }
 }
 
+function redarr(arr) {
+  arr = [...new Set(arr)].sort()
+  let obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    obj[i + ''] = arr[i]
+  }
+  return obj
+}
+
 
 
 
